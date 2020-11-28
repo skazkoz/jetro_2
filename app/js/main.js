5ce6709a0f2cad6df3e8a4e99693e74a799519c0
skazkoz <skazkoz@gmail.com>
2020-11-28 20:58:47 +0200

    add part of first slider

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    $('.slider_inner').slick({
+        // arrows: true,
+        dots: false,
+        prevArrow: '<button class="slick-arrow slick-prev" ><img src="../images/slider/slide-left.svg" alt=""></button>',
+        nextArrow: '  <button class="slick-arrow slick-next"><img src="../images/slider/slide-right.svg" alt=""></button>',
+    });
+});
