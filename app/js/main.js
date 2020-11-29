bc338046cd8f0479b71b89854fec362f093e9c53
skazkoz <skazkoz@gmail.com>
2020-11-29 12:49:27 +0200

    fix slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,13 @@
 $(function() {
 
     $('.slider_inner').slick({
+        аccessibility: false,
         // arrows: true,
-        dots: false,
-        prevArrow: '<button class="slick-arrow slick-prev" ><img src="../images/slider/slide-left.svg" alt=""></button>',
-        nextArrow: '  <button class="slick-arrow slick-next"><img src="../images/slider/slide-right.svg" alt=""></button>',
+        // dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev" ><img src="../images/slider/previous.svg" alt=""></button>',
+        nextArrow: '  <button class="slick-arrow slick-next"><img src="../images/slider/next.svg" alt=""></button>',
     });
+
 });
